0cc526e01e6cae290fc911365a2b62ede580295f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 18:46:53 +0100

    1

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ http.createServer(function (req, res) {
     fs.readFile('./demofile1.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
-
+        return res.end();
     });
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.write("</body>\n</html>");
-    res.end();
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    //res.write(" ");
+    //res.end();
 }).listen(1337);
 
 //Modu³y:
