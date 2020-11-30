8c3dcc7a23dab075710c08024553abbdcd65a502
BuildTools <unconfigured@null.spigotmc.org>
2020-11-30 15:09:00 +0100

    mongo+tekst

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var http = require('http');
 const port = process.env.PORT || 1337
 //var dt = require('./myfirstmodule');
 var fs = require('fs');
-//var mongo = require('mongodb');
+var mongo = require('mongodb');
 
 http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write("<html>\n<body>\n");
-
+    res.write("<p>Jakiœ tekst testowy i mongo</p>\n");
     res.write("</body>\n</html>");
     return res.end();
        
