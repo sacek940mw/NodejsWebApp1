d878e33a162a8a522f6f33c4b774ddbd54c6f324
BuildTools <unconfigured@null.spigotmc.org>
2020-11-30 11:31:23 +0100

    1

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var fs = require('fs');
 var mongo = require('mongodb');
 
 http.createServer(function (req, res) {
-    
-    fs.readFile('./demofile1.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<html>\n<body>");
+
+    res.write("</body>\n</html>");
+    return res.end();
+       
 }).listen(1337);
