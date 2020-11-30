8ee4f175609073a0ab1f99dd5f6ea3f6c07759b9
BuildTools <unconfigured@null.spigotmc.org>
2020-11-30 11:16:32 +0100

    2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 var http = require('http');
-//var dt = require('./myfirstmodule');
+var dt = require('./myfirstmodule');
 var fs = require('fs');
-//var mongo = require('mongodb');
+var mongo = require('mongodb');
 
 http.createServer(function (req, res) {
     
@@ -12,84 +12,3 @@ http.createServer(function (req, res) {
         return res.end();
     });
 }).listen(1337);
-
-//Modu³y:
-
-
-/*
-
-var insertDocument = function (db, callback) {
-    db.collection('families').insertOne({
-        "id": "AndersenFamily",
-        "lastName": "Andersen",
-        "parents": [
-            { "firstName": "Thomas" },
-            { "firstName": "Mary Kay" }
-        ],
-        "children": [
-            { "firstName": "John", "gender": "male", "grade": 7 }
-        ],
-        "pets": [
-            { "givenName": "Fluffy" }
-        ],
-        "address": { "country": "USA", "state": "WA", "city": "Seattle" }
-    }, function (err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the families collection.");
-        callback();
-    });
-};
-
-var findFamilies = function (db, callback) {
-    var cursor = db.collection('families').find();
-    cursor.each(function (err, doc) {
-        assert.equal(err, null);
-        if (doc != null) {
-            console.dir(doc);
-        } else {
-            callback();
-        }
-    });
-};
-
-var updateFamilies = function (db, callback) {
-    db.collection('families').updateOne(
-        { "lastName": "Andersen" },
-        {
-            $set: {
-                "pets": [
-                    { "givenName": "Fluffy" },
-                    { "givenName": "Rocky" }
-                ]
-            },
-            $currentDate: { "lastModified": true }
-        }, function (err, results) {
-            console.log(results);
-            callback();
-        });
-};
-
-var removeFamilies = function (db, callback) {
-    db.collection('families').deleteMany(
-        { "lastName": "Andersen" },
-        function (err, results) {
-            console.log(results);
-            callback();
-        }
-    );
-};
-
-MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
-    var db = client.db('familiesdb');
-    insertDocument(db, function () {
-        findFamilies(db, function () {
-            updateFamilies(db, function () {
-                removeFamilies(db, function () {
-                    client.close();
-                });
-            });
-        });
-    });
-});
-*/
