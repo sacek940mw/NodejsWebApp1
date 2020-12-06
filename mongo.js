35988881b4b124f1b1fc5764ca2c294eb685d097
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 17:37:56 +0100

    1

--- mongo.js
+++ mongo.js
@@ -2,15 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://cosmosace:u1Jdn9vibrTgQo3hvbTTCnKoIr9BB3qLpT91Z8Vt9WpC1lQigxyNfkntjtyA71MaMXcNuToDZwsHqw4YyCFQ2w%3D%3D@cosmosace.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cosmosace@";
 //var Promise = require('rsvp').Promise;
 
-exports.dodajHaslo = function (myobj) {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("strona");
-        //var myobj = { name: "Company Inc", address: "Highway 37" };
-        dbo.collection("Hasla").insertOne(myobj, function (err, res) {
+exports.dodajHaslo = function (myobj, callback) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, function (err, db) {
             if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
+            var dbo = db.db("strona");
+            //var myobj = { name: "Company Inc", address: "Highway 37" };
+            dbo.collection("Hasla").insertOne(myobj, function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+                resolve("1 document inserted");
+            });
         });
     });
 };
--- server.js
+++ server.js
@@ -1,81 +1,13 @@
 'use strict';
 var http = require('http');
+var url = require('url');
 const port = process.env.PORT || 1337
 //var dt = require('./myfirstmodule');
 var fs = require('fs');
 const mon = require('./mongo');
+const stron = require('./strona');
 const { parse } = require('querystring');
 
 var hasla = [];
 
-var prom = mon.znajdzHasla();
-var waited = prom.then((value) => {
-    //console.log(value[0]);
-    hasla = value;
-});
-
-
-http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        waited = req.on('end', () => {
-            console.log(
-                parse(body)
-            );
-            waited = mon.dodajHaslo(parse(body));
-        });
-    }
-    waited = console.log("write");
-    res.write(`
-        <!doctype html>
-        <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Manager hase³</title>
-        </head>
-        <body>            
-            <h1 align="center">Dodaj has³o</h1>  
-            <div class="formularz" align="center">
-                <form align=\"center\" action="/" method="post">
-                    <label for="strona">Strona:</label><br>
-                    <input type="text" name="strona" /><br>
-                    <label for="login">Login:</label><br>
-                    <input type="text" name="login" /><br>
-                    <label for="haslo">Has³o:</label><br>
-                    <input type="text" name="haslo" /><br>
-                    <button>Dodaj</button>
-                </form>
-                <br>
-            </div>`);
-    res.write(`
-            <div class="hasla" align="center">
-                <br>
-                <table align=\"center\" border=\"1\">
-                  <tr>
-                    <th>Strona: </th>
-                    <th>Login: </th>
-                    <th>Has³o: </th>
-                  </tr>
-        `);
-    var i;
-    for (i = 0; i < hasla.length; i++) {
-        console.log(hasla[i]);
-        res.write('<tr>');
-        res.write('<th>' + hasla[i].strona + '</th>');
-        res.write('<th>' + hasla[i].login + '</th>');
-        res.write('<th>' + hasla[i].haslo + '</th>');
-        res.write('<tr>');
-    }
-    res.write(`
-                    </table>
-                </div>
-        `);
-    res.end(`
-        </body>
-        </html>
-        `);
-
-}).listen(port);
+var serwer = stron.stwSer();
